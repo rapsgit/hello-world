b9fab0bdd92e6a4650dacce513cb1d8806839a0c
Monika Sperr <monika@kranich>
2015-12-02 16:31:39 +0100

    Juhui

--- hello-world.c
+++ hello-world.c
@@ -9,5 +9,6 @@ int main()
   printf("Ich seh ich immer noch nichts!\n");
   printf("Jetzt Ich seh ich was!\n");
   printf("So, das ist jetzt die letzte Anpassung!\n");
+  printf("Juhui!!!!\n");
   return 0;
 }
